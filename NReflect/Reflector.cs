1b88f90ab365c9e07ee53f572ee5906fb4846931
Igor Zubkov <igor.z@oneupgames.gg>
2025-02-14 17:43:55 +0300

    upgrade to .net8

--- NReflect/Reflector.cs
+++ NReflect/Reflector.cs
@@ -22,121 +22,92 @@ using NReflect.Filter;
 
 namespace NReflect
 {
-  /// <summary>
-  /// An instance of this class is able to reflect the types of an assembly.
-  /// </summary>
-  /// <remarks>
-  /// If reflection takes place inside the same app domain it is called from, the
-  /// reflected assembly files stay opened until this app domain is unloaded. If
-  /// the current app domain is the starting app domain, this can only be done via
-  /// closing the application.
-  /// </remarks>
-  public class Reflector : MarshalByRefObject
-  {
-
-    // ========================================================================
-    // Properties
-
-    #region === Properties
-
     /// <summary>
-    /// Gets or sets the type filter used to determine which types to reflect.
+    /// An instance of this class is able to reflect the types of an assembly.
     /// </summary>
-    private IFilter Filter { get; set; }
-
-    #endregion
-
-    // ========================================================================
-    // Methods
-
-    #region === Methods
-
-    /// <summary>
-    /// Reflects the types of an assembly.
-    /// </summary>
-    /// <param name="fileName">The file name of the assembly to reflect.</param>
-    /// <param name="useNewAppDomain">If set to true, a new app domain will be used for
-    ///                               the reflection.</param>
-    /// <returns>The result of the reflection.</returns>
-    public NRAssembly Reflect(string fileName, bool useNewAppDomain = true)
+    /// <remarks>
+    /// If reflection takes place inside the same app domain it is called from, the
+    /// reflected assembly files stay opened until this app domain is unloaded. If
+    /// the current app domain is the starting app domain, this can only be done via
+    /// closing the application.
+    /// </remarks>
+    public class Reflector : MarshalByRefObject
     {
-      IFilter filter = null;
-      return Reflect(fileName, ref filter, useNewAppDomain);
+        // ========================================================================
+        // Properties
+
+        #region === Properties
+
+        /// <summary>
+        /// Gets or sets the type filter used to determine which types to reflect.
+        /// </summary>
+        private IFilter Filter { get; set; }
+
+        #endregion
+
+        // ========================================================================
+        // Methods
+
+        #region === Methods
+
+        /// <summary>
+        /// Reflects the types of an assembly.
+        /// </summary>
+        /// <param name="fileName">The file name of the assembly to reflect.</param>
+        /// <param name="filter">The type filter used to determine which types to reflect.</param>
+        /// <returns>The result of the reflection.</returns>
+        public NRAssembly Reflect(string fileName, ref IFilter filter, out string[] errors)
+        {
+            Filter = filter;
+            return Reflect(fileName, out errors);
+        }
+
+        /// <summary>
+        /// Reflects the types of an assembly.
+        /// </summary>
+        /// <param name="fileName">The file name of the assembly to reflect.</param>
+        /// <returns>The result of the reflection.</returns>
+        private NRAssembly Reflect(string fileName, out string[] errors)
+        {
+            ReflectionWorker reflectionWorker = new ReflectionWorker
+            {
+                Filter = Filter ?? new ReflectAllFilter()
+            };
+            
+            var nrAssembly = reflectionWorker.Reflect(fileName);
+            errors = reflectionWorker.Errors;
+
+            return nrAssembly;
+        }
+
+        /// <summary>
+        /// Reflects the types of the provided assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly to reflect.</param>
+        /// <returns>The result of the reflection.</returns>
+        public NRAssembly Reflect(Assembly assembly)
+        {
+            IFilter filter = null;
+            return Reflect(assembly, ref filter);
+        }
+
+        /// <summary>
+        /// Reflects the types of the provided assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly to reflect.</param>
+        /// <param name="filter">The type filter used to determine which types to reflect.</param>
+        /// <returns>The result of the reflection.</returns>
+        public NRAssembly Reflect(Assembly assembly, ref IFilter filter)
+        {
+            Filter = filter;
+            ReflectionWorker reflectionWorker = new ReflectionWorker
+            {
+                Filter = Filter ?? new ReflectAllFilter()
+            };
+
+            return reflectionWorker.Reflect(assembly);
+        }
+
+        #endregion
     }
-
-    /// <summary>
-    /// Reflects the types of an assembly.
-    /// </summary>
-    /// <param name="fileName">The file name of the assembly to reflect.</param>
-    /// <param name="filter">The type filter used to determine which types to reflect.</param>
-    /// <param name="useNewAppDomain">If set to true, a new app domain will be used for
-    ///                               the reflection.</param>
-    /// <returns>The result of the reflection.</returns>
-    public NRAssembly Reflect(string fileName, ref IFilter filter, bool useNewAppDomain = true)
-    {
-      if (useNewAppDomain)
-      {
-        // Create the new AppDomain
-        AppDomain newAppDomain = AppDomain.CreateDomain("NReflect-AppDomain", null, null);
-        // Create a new instance of Reflector at the new AppDomain
-        String dllName = Assembly.GetAssembly(typeof(Reflector)).FullName;
-        Reflector reflector = (Reflector)newAppDomain.CreateInstanceAndUnwrap(dllName, "NReflect.Reflector");
-        // Move the filter from this to the new instance
-        reflector.Filter = filter;
-
-        NRAssembly nrAssembly = reflector.Reflect(fileName);
-        // Move the filter back from the new to this instance
-        filter = reflector.Filter;
-
-        AppDomain.Unload(newAppDomain);
-
-        return nrAssembly;
-      }
-      return Reflect(fileName);
-    }
-
-    /// <summary>
-    /// Reflects the types of an assembly.
-    /// </summary>
-    /// <param name="fileName">The file name of the assembly to reflect.</param>
-    /// <returns>The result of the reflection.</returns>
-    private NRAssembly Reflect(string fileName)
-    {
-      ReflectionWorker reflectionWorker = new ReflectionWorker
-                                            {
-                                              Filter = Filter ?? new ReflectAllFilter()
-                                            };
-
-      return reflectionWorker.Reflect(fileName);
-    }
-
-    /// <summary>
-    /// Reflects the types of the provided assembly.
-    /// </summary>
-    /// <param name="assembly">The assembly to reflect.</param>
-    /// <returns>The result of the reflection.</returns>
-    public NRAssembly Reflect(Assembly assembly)
-    {
-      IFilter filter = null;
-      return Reflect(assembly, ref filter);
-    }
-
-    /// <summary>
-    /// Reflects the types of the provided assembly.
-    /// </summary>
-    /// <param name="assembly">The assembly to reflect.</param>
-    /// <param name="filter">The type filter used to determine which types to reflect.</param>
-    /// <returns>The result of the reflection.</returns>
-    public NRAssembly Reflect(Assembly assembly, ref IFilter filter)
-    {
-      ReflectionWorker reflectionWorker = new ReflectionWorker
-      {
-        Filter = Filter ?? new ReflectAllFilter()
-      };
-
-      return reflectionWorker.Reflect(assembly);
-    }
-
-    #endregion
-  }
 }
